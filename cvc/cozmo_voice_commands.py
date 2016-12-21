a3ad8ff49a296ba282819b2e4d22a0666c41d9b9
Riccardo Sallusti <riccardo.sallusti@gmail.com>
2016-12-21 01:02:05 +0100

    Added new very advanced parser for commands

--- cvc/cozmo_voice_commands.py
+++ cvc/cozmo_voice_commands.py
@@ -50,16 +50,17 @@ def run(sdk_conn):
         robot = None
 
     vc = voice_commands.VoiceCommands(robot)
-
+    if robot:
+        robot.play_anim("anim_cozmosays_getout_short_01").wait_for_completed()
+    set_language()
+    cprint("You can give voice commands to Cozmo. Available Commands are:\n" + str(get_supported_commands()), "green")
     try:
-        set_language()
-        cprint("You can give voice commands to Cozmo. Available Commands are:\n" + str(get_supported_commands()), "green")
         with sr.Microphone(chunk_size=512) as source:
             while 1:
                 if robot:
                     checkBattery(robot)
                     flash_backpack(robot, True)
-                    #robot.say_text(text="", play_excited_animation=True).wait_for_completed()
+                    robot.play_anim("anim_meetcozmo_getin").wait_for_completed()
                 print("\nSay something (ctrl+c to exit):")
                 hear(source, robot)
     except KeyboardInterrupt:
@@ -105,19 +106,31 @@ def checkBattery(robot):
 def get_supported_commands():
     '''Construct a list of all methods in this class that start with 'lang' variabler content - these are commands we accept'''
     prefix_str = lang + "_"
-    prefix_len = len(prefix_str)
     supported_commands = []
-    for func_name in dir(vc.__class__):
+    for func_name in dir(vc):
         if func_name.startswith(prefix_str):
-            supported_commands.append(func_name[prefix_len:])
+            supported_commands.append(func_name[len(prefix_str):])
     return supported_commands
 
 def get_command(command_name):
     '''Find a matching function inside 'VoiceCommands' class and return it. return None if there's no match'''
-    try:
-        return getattr(vc, lang + "_" + command_name.lower()) #here the magic happens
-    except AttributeError:
-        return None
+    cprint(command_name, "red")
+    prefix_str = lang + "_"
+    for func_name in dir(vc):
+        '''cycle through all the methods in vc and look for one that is contained in the command as a substring!
+        this allows us to use similar words like drive o driving to be executed as well!'''
+        if func_name.startswith(prefix_str):
+            splitted = func_name[len(prefix_str):len(func_name)-1] #get only the right part
+            #print(splitted)
+            if splitted in command_name.lower():
+                #cprint("FOUND: " + func_name, "green")
+                return getattr(vc, func_name)
+    return None
+
+    # try:
+    #     return getattr(vc, lang + "_" + command_name.lower()) #here the magic happens
+    # except AttributeError:
+    #     return None
 
 def extract_command_from_string(in_string):
     '''Separate inString at each space, loop through until we find a command, return tuple of cmd_func and cmd_args'''
@@ -130,7 +143,6 @@ def extract_command_from_string(in_string):
 
         if cmd_func:
             cmd_args = split_string[i + 1:]
-            #cmd_first_int = re.search(r'\d+', cmd_args).group()
             return cmd_func, cmd_args
 
     # No valid command found
@@ -160,8 +172,10 @@ def hear(source, robot):
             else:
                 cprint("Sorry I don't understand your command, available commands are:", "red")
                 cprint(str(get_supported_commands()), "green")
+                robot.play_anim("anim_pounce_reacttoobj_01_shorter").wait_for_completed()
         else:
             cprint("You did not say the magic word " + command_activate, "red")
+            robot.play_anim("anim_pounce_reacttoobj_01_shorter").wait_for_completed()
 
     except sr.UnknownValueError:
         cprint("Google Speech Recognition could not understand audio", "red")
