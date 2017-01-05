f365f33f1e8514ff70757546d2ffedf4741966f6
Riccardo Sallusti <riccardo.sallusti@gmail.com>
2017-01-05 01:14:58 +0100

    Version 0.4.0 - Back to Charger COMMAND added

--- cvc/cozmo_voice_commands.py
+++ cvc/cozmo_voice_commands.py
@@ -21,7 +21,7 @@ except ImportError:
 from . import voice_commands
 
 ###### VARS ######
-title = "Cozmo-Voice-Commands (CvC) - Version 0.3.6"
+title = "Cozmo-Voice-Commands (CvC) - Version 0.4.0"
 author =" - Riccardo Sallusti (http://riccardosallusti.it)"
 log = False
 lang = "en"
