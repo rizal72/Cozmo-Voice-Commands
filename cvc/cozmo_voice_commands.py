273b09b8ec7944af762d84b0c75cbc8d161594a3
rizal72 <riccardo.sallusti@gmail.com>
2017-01-10 11:03:59 +0100

    Version 0.4.2

--- cvc/cozmo_voice_commands.py
+++ cvc/cozmo_voice_commands.py
@@ -21,7 +21,7 @@ except ImportError:
 from . import voice_commands
 
 ###### VARS ######
-title = "Cozmo-Voice-Commands (CvC) - Version 0.4.1"
+title = "Cozmo-Voice-Commands (CvC) - Version 0.4.2"
 author =" - Riccardo Sallusti (http://riccardosallusti.it)"
 log = False
 lang = "en"
