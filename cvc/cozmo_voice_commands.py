a1770b6c459861cafb04a4e39610e05827803a23
rizal72 <riccardo.sallusti@gmail.com>
2017-01-19 13:10:48 +0100

    added manifest

--- cvc/cozmo_voice_commands.py
+++ cvc/cozmo_voice_commands.py
@@ -89,7 +89,7 @@ def run(robot: cozmo.robot.Robot):
 def load_jsons():
     global languages
     cprint("loading languages files...","yellow")
-    files_location = os.path.dirname(os.path.realpath(__file__)) + '/languages'
+    files_location = 'languages/*.json' #os.path.dirname(os.path.realpath(__file__)) + '/languages/*.json'
     if log:
         print("Files Location: "+files_location)
     for file in glob.glob(files_location):
